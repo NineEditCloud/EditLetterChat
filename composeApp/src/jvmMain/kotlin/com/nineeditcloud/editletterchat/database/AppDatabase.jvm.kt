ba3792c5e0adb94129f2d383a8340f2a54affe3f
Administrator <editcloud1790316228@outlook.com>
2026-05-22 19:53:54 +0800

    Initial commit

--- composeApp/src/jvmMain/kotlin/com/nineeditcloud/editletterchat/database/AppDatabase.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/nineeditcloud/editletterchat/database/AppDatabase.jvm.kt
@@ -1,10 +1,18 @@
 package com.nineeditcloud.editletterchat.database
 
-//import androidx.room.Room
-//import androidx.room.RoomDatabase
-//import java.io.File
-//
-//actual fun getDatabaseBuilder/*JVM桌面平台-获取数据库构建器*/(dbName:String):RoomDatabase.Builder<AppDatabase>{
-//    val dbFile=File(System.getProperty("java.io.tmpdir"), "$dbName.db")
-//    return Room.databaseBuilder<AppDatabase>(name=dbFile.absolutePath)
-//}
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
+import java.io.File
+
+actual fun getDatabaseBuilder/*JVM桌面平台-获取数据库构建器*/(dbName:String):RoomDatabase.Builder<AppDatabase>{
+    val dbFile=File(System.getProperty("java.io.tmpdir"), "$dbName.db")
+    return Room.databaseBuilder<AppDatabase>(name=dbFile.absolutePath)
+}
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual object AppDatabaseConstructor:RoomDatabaseConstructor<AppDatabase> {
+    actual override fun initialize():AppDatabase {
+        TODO("Not yet implemented")
+    }
+}
